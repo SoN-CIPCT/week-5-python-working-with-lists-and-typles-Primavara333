69b28d819611bc570635fc7911374f57f843a95a
Primavara333 <mary.gherman3@gmail.com>
2025-02-10 19:51:37 -0800

    entire homework now

--- week5.py
+++ week5.py
@@ -2,7 +2,12 @@ cities = ['Paris', 'London', 'Madrid', 'New York', 'Seattle', 'Oslo']
 print(cities[0])
 print(cities[0:2])
 print(cities[2:4])
-      
-print(cities[0:4])
-print(cities[0:-4])
-print(cities[0:5:])
+print(cities[0:6:5])
+menu = ("Pizza", "Burger", "Pasta", "Salad", "Soup")
+print("Original Menu:")
+for food in menu:
+    print(food)
+updated_menu = ("Pizza", "Burger", "Sushi", "Tacos", "Steak")
+print("\nRevised Menu:")
+for food in updated_menu:
+    print(food)
